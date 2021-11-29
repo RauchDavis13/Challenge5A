56343a803e70a539b0d4dfed532b3b2fd05f13e0
KRD13 <rauchdavis13@gmail.com>
2021-11-29 18:27:02 -0500

    making changes

--- script.js
+++ script.js
@@ -1,51 +1,41 @@
+$(document).ready(function() {
+  $(".saveBtn").on("click", function() {
+
 // tracks current date information for header
 var todayDate = moment();
 // tracks current hour for comparison to hour time blocks in function timeCheck
-var hourCurrent = moment().hour();
+
 var today = $("#currentDay");
 
 // reconfigures date format from "moments" to correct format for header from mockup....the specified format was not found in "moments"
-$(document).ready(function() {
-  dayName = todayDate.format("dddd")
-  month = todayDate.format("MMMM")
-  day = todayDate.format("Do")
-  today.text(dayName + ", " + month + " " + day);
-})
-
-$(document).ready(function() {
 
    // Save button click listener
-  $(".saveBtn").on("click", function() {
+  
     
     // get parent element id from button click
-    var hourId = $( this )
-      .parent()
-      .get(0)
-      .id;
+    var hourId = $(this).parent().attr("id");
     
     // get text from sibling textarea element to the button
-    var text = $(this)
-    .siblings("#timeText")
-    .val();
+    var text = $(this).siblings(".description").val();
 
   // creat local storage
   localStorage.setItem(hourId, text);
   });
 
-  $("#time8 #timeText").val(localStorage.getItem("time8"));
-  $("#time9 #timeText").val(localStorage.getItem("time9"));
-  $("#time10 #timeText").val(localStorage.getItem("time10"));
-  $("#time11 #timeText").val(localStorage.getItem("time11"));
-  $("#time12 #timeText").val(localStorage.getItem("time12"));
-  $("#time1 #timeText").val(localStorage.getItem("time1"));
-  $("#time2 #timeText").val(localStorage.getItem("time2"));
-  $("#time3 #timeText").val(localStorage.getItem("time3"));
-  $("#time4 #timeText").val(localStorage.getItem("time4"));
-  $("#time5 #timeText").val(localStorage.getItem("time5"));
-  $("#time6 #timeText").val(localStorage.getItem("time6"));
+  $("#time8 .description").val(localStorage.getItem("time8"));
+  $("#time9 .description").val(localStorage.getItem("time9"));
+  $("#time10 .description").val(localStorage.getItem("time10"));
+  $("#time11 .description").val(localStorage.getItem("time11"));
+  $("#time12 .description").val(localStorage.getItem("time12"));
+  $("#time1 .description").val(localStorage.getItem("time1"));
+  $("#time2 .description").val(localStorage.getItem("time2"));
+  $("#time3 .description").val(localStorage.getItem("time3"));
+  $("#time4 .description").val(localStorage.getItem("time4"));
+  $("#time5 .description").val(localStorage.getItem("time5"));
+  $("#time6 .description").val(localStorage.getItem("time6"));
 
   function timeCheck() {
-       
+    var hourCurrent = moment().hour();
     // loop over time-blocks to identify "hour" to compare against current time and style to past, present, future
     $(".time-block").each(function () {
         //identifies and uses hour value from timeblock id for comparison to current time
@@ -53,24 +43,21 @@ $(document).ready(function() {
         console.log(timeBlock);
         // compares timeb-lock hour to current hour and identifies and styles timeblocks in the past
         if (timeBlock < hourCurrent) {
-            $(this).find("#timeText").removeClass("future");
-            $(this).find("#timeText").removeClass("present");
-            $(this).find("#timeText").addClass("past");
-        }
+            $(this).addClass('past');
+         }
         // compares time-block hour to current hour and identifies and style timeblock in the present hour
         else if (timeBlock === hourCurrent) {
-            $(this).find("#timeText").removeClass("past");
-            $(this).find("#timeText").removeClass("future");
-            $(this).find("#timeText").addClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("present");
         }
         // compares time-block hour to current hour and identifies and style timeblocks in the future
         else {
-            $(this).find("#timeText").removeClass("present");
-            $(this).find("#timeText").removeClass("past");
-            $(this).find("#timeText").addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
 
         }
-    })
+    });
   }
 timeCheck();
 })

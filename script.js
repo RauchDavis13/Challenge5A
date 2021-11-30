20ea2b332d97f9217b202cff6dd401bcd6b9da58
KRD13 <rauchdavis13@gmail.com>
2021-11-29 20:41:20 -0500

    had to redo the date showing in the header

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 $(document).ready(function() {
+  // Save button click listener
   $(".saveBtn").on("click", function() {
 
-// tracks current date information for header
-var todayDate = moment();
-// tracks current hour for comparison to hour time blocks in function timeCheck
-
-var today = $("#currentDay");
-
 // reconfigures date format from "moments" to correct format for header from mockup....the specified format was not found in "moments"
-
-   // Save button click listener
-  
     
     // get parent element id from button click
     var hourId = $(this).parent().attr("id");
@@ -21,6 +13,39 @@ var today = $("#currentDay");
   // creat local storage
   localStorage.setItem(hourId, text);
   });
+
+  
+
+  function timeCheck() {
+    var hourCurrent = moment().hour();
+    // loop over time-blocks to identify "hour" to compare against current time and style to past, present, future
+    $(".time-block").each(function () {
+        //identifies and uses hour value from timeblock id for comparison to current time
+        var timeBlock = parseInt($(this).attr("id").split("time")[1]);
+        console.log(timeBlock);
+
+        // compares timeb-lock hour to current hour and identifies and styles timeblocks in the past
+        if (timeBlock < hourCurrent) {
+            $(this).find(".description").addClass('past');
+         }
+        // compares time-block hour to current hour and identifies and style timeblock in the present hour
+        else if (timeBlock === hourCurrent) {
+            $(this).find(".description").removeClass("past");
+            $(this).find(".description").addClass("present");
+        }
+        // compares time-block hour to current hour and identifies and style timeblocks in the future
+        else {
+            $(this).find(".description").removeClass("past");
+            $(this).find(".description").removeClass("present");
+            $(this).find(".description").addClass("future");
+        }
+    });
+  }
+  timeCheck();
+
+  var interval = setInterval(timeCheck, 15000);
+    
+
 
   $("#time8 .description").val(localStorage.getItem("time8"));
   $("#time9 .description").val(localStorage.getItem("time9"));
@@ -34,30 +59,8 @@ var today = $("#currentDay");
   $("#time5 .description").val(localStorage.getItem("time5"));
   $("#time6 .description").val(localStorage.getItem("time6"));
 
-  function timeCheck() {
-    var hourCurrent = moment().hour();
-    // loop over time-blocks to identify "hour" to compare against current time and style to past, present, future
-    $(".time-block").each(function () {
-        //identifies and uses hour value from timeblock id for comparison to current time
-        var timeBlock = parseInt($(this).attr("id").split("time")[1]);
-        console.log(timeBlock);
-        // compares timeb-lock hour to current hour and identifies and styles timeblocks in the past
-        if (timeBlock < hourCurrent) {
-            $(this).addClass('past');
-         }
-        // compares time-block hour to current hour and identifies and style timeblock in the present hour
-        else if (timeBlock === hourCurrent) {
-            $(this).removeClass("past");
-            $(this).addClass("present");
-        }
-        // compares time-block hour to current hour and identifies and style timeblocks in the future
-        else {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-        }
-    });
-  }
-timeCheck();
-})
+});
+
+
